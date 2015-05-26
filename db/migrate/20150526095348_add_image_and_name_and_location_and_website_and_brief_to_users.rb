002598d9ae55253e0043101a815fa1b0a0756d3f
danizraikat <danizraikat@gmail.com>
2015-05-26 10:58:34 +0100

    added new columns to user table, migrated

---
+++ db/migrate/20150526095348_add_image_and_name_and_location_and_website_and_brief_to_users.rb
@@ -0,0 +1,9 @@
+class AddImageAndNameAndLocationAndWebsiteAndBriefToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :image, :string
+    add_column :users, :name, :string
+    add_column :users, :location, :string
+    add_column :users, :website, :text
+    add_column :users, :brief, :text
+  end
+end
