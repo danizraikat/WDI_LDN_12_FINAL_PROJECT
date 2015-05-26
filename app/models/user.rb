8f3da3840ed58198f95cb98ad5e5945a0f469b0a
danizraikat <danizraikat@gmail.com>
2015-05-26 11:08:47 +0100

    added role column to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526095348) do
+ActiveRecord::Schema.define(version: 20150526100800) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150526095348) do
     t.string   "location"
     t.text     "website"
     t.text     "brief"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
