4608da67574b2047509aedcb77c4192747dee457
danizraikat <danizraikat@gmail.com>
2015-05-26 12:06:05 +0100

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526100800) do
+ActiveRecord::Schema.define(version: 20150526110432) do
+
+  create_table "tests", force: :cascade do |t|
+    t.string   "image"
+    t.string   "name"
+    t.text     "brief"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
