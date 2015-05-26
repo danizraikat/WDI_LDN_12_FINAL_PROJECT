94f4fe471a59c1af6c6fbde5e19b9db6bc3a7586
danizraikat <danizraikat@gmail.com>
2015-05-26 11:51:44 +0100

    user model validations tested successfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :name, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
+  validates :name, length: { maximum: 30,
+    too_long: "%{count} characters is the maximum allowed"}
   validates :role, inclusion: { in: %w(Admin User),
     message: "%{value} is not accepted as a role" }
   validates :brief, length: { maximum: 500,
