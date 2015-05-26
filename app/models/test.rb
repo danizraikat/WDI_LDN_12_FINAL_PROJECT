f54705739abd5fbfedb3991558bf75fd7491caf8
danizraikat <danizraikat@gmail.com>
2015-05-26 12:15:26 +0100

    test model validations teste

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ActiveRecord::Base
+  validates :brief, length: { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :name, length: { maximum: 50,
+    too_long: "%{count} characters is the maximum allowed"}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 user1 = User.create!(email: "dani@dani.com", password: "password", name: "Dani Al-Zraykat", brief: "Happy to be the first user of this website", role: "Admin")
+
+test1 = Test.create!(image:"http://www.javatpoint.com/images/javascript/javascript_logo.png", name: "JavaScript", brief: "First test on the website")
