09960c657b9f1ea39e37eb1b710def48486bc859
danizraikat <danizraikat@gmail.com>
2015-05-26 11:37:40 +0100

    added 3 validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
+  validates :role, inclusion: { in: %w(Admin User),
+    message: "%{value} is not accepted as a role" }
+  validates :brief, length: { maximum: 500,
+    too_long: "%{count} characters is the maximum allowed" }
 end
