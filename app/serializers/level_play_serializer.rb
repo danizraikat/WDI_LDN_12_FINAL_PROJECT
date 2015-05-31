4669035713c80bd61e69dcbbf48e18ebcd0bd06e
danizraikat <danizraikat@gmail.com>
2015-05-31 12:31:23 +0100

    added and removed level model in backbone...can't get level_play button to navigat eto the right level_play page

--- app/serializers/level_play_serializer.rb
+++ app/serializers/level_play_serializer.rb
@@ -1,4 +1,5 @@
 class LevelPlaySerializer < ActiveModel::Serializer
+  has_one :level, except: [:level_plays]
   has_many :question_plays
   has_many :questions 
   attributes :id, :user_id, :level_id, :score 
